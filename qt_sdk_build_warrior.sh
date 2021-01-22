e6439462f5f92f06ff855cf9e94384b07032eca9
Valeev Bulat <vbm@eleps.ru>
2021-01-22 16:15:11 +0300

    fixed

--- qt_sdk_build_warrior.sh
+++ qt_sdk_build_warrior.sh
@@ -5,7 +5,7 @@ HASH_SOLIDRUN=d567d75be8984bd868df9af6108f12fecb049c2d
 HASH_FREESCALE=affb4b17d3d97ae45bdc4887268ba1d88fccd876
 HASH_OPENEMBEDDED=dacfa2b1920e285531bec55cd2f08743390aaf57
 HASH_QT5_EXTRA=79e26686520f2ce5f743975e90116b263a33697f
-HASH_ELEPS=b0ea942d54695d44ac104dbaa66abfcda05be27c
+#HASH_ELEPS=b0ea942d54695d44ac104dbaa66abfcda05be27c
 git clone -b rocko https://github.com/openembedded/openembedded-core.git
 cd openembedded-core && git checkout $HASH_CORE
 git clone -b 1.36 https://github.com/openembedded/bitbake.git
@@ -22,7 +22,7 @@ cd meta-solidrun-arm-imx6 && git checkout $HASH_SOLIDRUN && cd ..
 cd meta-freescale && git checkout $HASH_FREESCALE && cd ..
 cd meta-openembedded && git checkout $HASH_OPENEMBEDDED && cd ..
 cd meta-qt5-extra && git checkout $HASH_QT5_EXTRA && cd ..
-cd meta-eleps && git checkout $HASH_ELEPS && cd ..
+#cd meta-eleps && git checkout $HASH_ELEPS && cd ..
 
 
 
